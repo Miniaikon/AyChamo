199a699b58c49dd9be40e08c8791ef59429acc13
Ysmael <skategrama@gmail.com>
2016-02-03 15:04:30 +0100

    Diseño páginacion 2.0

---
+++ paginacion/index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Desarrolladores de sitios web.</title>
+</head>
+<body>
+
+<header class="col-md-12">
+	<div class="col-md-8">
+		<h1>Purefull</h1>
+	</div>
+	<div class="col-md-4"><br>
+		<ul class="nav nav-pills">
+		  <li role="presentation"><a href="#">Home</a></li> 
+		  <li role="presentation"><a href="#">Profile</a></li> 
+		  <li role="presentation"><a href="#">Messages</a></li> 
+		</ul>
+	</div>
+</header>
+<div class="pedazo col-md-12">
+	<div class="row presentacion">
+		<div class="col-md-4">
+			<div class="title col-md-12"><h1><span class="glyphicon glyphicon-blackboard"></span> Presentación</h1></div>
+			<p><br><br><span class="glyphicon glyphicon-hand-right"></span> Diseño temático a pedido.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Informacion completa de su empresa<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Dominio .com.ve por un año.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Alojamiento mínimo.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Posicionamiento básico en SEO. <br><br>
+			<button type="button" class="btn btn-default pull-right">Más información</button></p>
+		</div>
+		<div class="col-md-4">
+			<div class="title col-md-12"><h1><span class="glyphicon glyphicon-comment"></span> Blog/Noticias</h1></div>
+			<p><br><br><span class="glyphicon glyphicon-hand-right"></span> Aplicacion de publicaciones personalizadas.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Diseño adaptable a moviles.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Dominio .com.ve por un año.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Alojamiento mínimo de 10gb.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Posicionamiento en SEO. <br><br>
+			<button type="button" class="btn btn-default pull-right">Más información</button></p>
+		</div>
+		<div class="col-md-4">
+			<div class="title col-md-12"><h1><span class="glyphicon glyphicon-shopping-cart"></span> E-comerce</h1></div>
+			<p><br><br><span class="glyphicon glyphicon-hand-right"></span> Aplicacion de carrito de compras.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Diseño adaptable a moviles.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Dominio .com.ve por un año.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Alojamiento mínimo de 10gb.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Posicionamiento en SEO. <br><br>
+			<button type="button" class="btn btn-default pull-right">Más información</button></p>
+		</div>
+	</div>
+</div>
+<section class="col-md-12">
+	<h1>Bienvenido a <b>Paginacion.com.ve</b></h1><hr>
+	<div class="row">
+	<div class="col-md-1"></div>
+		<div class="col-md-4 text-justify"><center class="text-info"><h2>¿Quienes somos?</h2></center>
+			<p><b>Paginacion</b> es una empresa venezolana, nacida para diseñar su sitio web con las especificaciones de su preferencia. Contamos con un equipo de profesionales en desarrollo web y diseño de sistemas de información automatizados, listos para convertir la página de sus sueños en realidad. 
+</p>
+		</div>
+		<div class="col-md-2"></div>
+		<div class="col-md-4 text-justify"><center class="text-info"><h2>¿Porqué internet?</h2></center>
+			<p>En la actualidad el 68% de las personas usan internet para informarse sobre un servicio o producto antes de adquirirlo. Un sitio web le posibilita a su empresa tener: 
+	<ul>
+	<li style="list-style:none">-Alcance mundial.</li>
+	<li style="list-style:none">-Garantía de su prestigio y calidad.</li>
+	<li style="list-style:none">-Tarjeta de presentación.</li>
+	<li style="list-style:none">-Credibilidad.</li>
+	</ul>
+</p>
+		</div>
+		<div class="col-md-1"></div>
+	</div>
+</section><br>
+<div class="divider col-md-12"><h1><span class="fa fa-user-plus"></span> Servicios</h1></div>
+<br>
+<section class="col-md-12">
+	<div class="row container-fluid">
+		<div class="col-md-4 text-justify"><center><h2 class="text-info">Dominio</h2></center>
+			<p>	El dominio es la dirección en internet mediante la cual se accede al sitio web. Usted decide qué dirección quiere (varía según la disponibilidad) y con extensión “.com.ve”. <br>
+ 
+	<br>Nuestro equipo se encargará de las transacciones para comprar o renovar su dominio. El dominio está disponible durante todo un año desde el momento del pago. 
+</p>
+		</div>
+		<div class="col-md-4 text-justify"><center><h2 class="text-info">Diseño</h2></center>
+			<p>Para hacer su sitio web llamativo, funcional, con un diseño fluido y sencillo  trabajaremos de la mano con su empresa y las necesidades de la misma, tomando en cuenta el contenido que se muestre en la dirección de internet.<br><br>
+	Sus visitantes encontrarán la información que buscan con facilidad. Tener una buena experiencia al visitar su sitio web es un gran aval de la calidad del servicio y producto que usted ofrece. <br><br>
+	Nosotros le ofrecemos lo mejor: una página web que se adapte a sus necesidades y cuente con todas las comodidades. <br><br>
+</p>
+		</div>
+		<div class="col-md-4 text-justify"><center><h2 class="text-info">Atención personalizado</h2></center>
+			<p>Durante un período de tres (3) meses de contratado el servicio de Paginacion, le estaremos ofreciendo una atención personalizada para solucionar, agregar o retirar contenido de su página. </p>
+		</div>
+	</div>
+</section>
+<div class="divider col-md-12"><h1><span class="glyphicon glyphicon-folder-close"></span> Portafolios</h1></div>
+<section class="col-md-12"><br><br>
+	<div class="row">
+	 <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<span class="boton"><a href="#">Miniaikon</a></span>
+	    </div>
+	  </div>
+	    
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<span class="boton"><a href="#">Miniaikon</a></span>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<span class="boton"><a href="#">Miniaikon</a></span>
+	    </div>
+	  </div>
+	</div>
+
+</section>
+<footer class="final">
+	<div class="col-md-4" style="background:rgba(72,53,94,0.8);border-right:1px solid #7f8c8d; height:230px;">
+		<span style="color:#FFF;font-size:32px;"><strong>Redes</strong></span>
+		<div social class="btn-group-vertical btn-block" role="group">
+		  <a href="#" class="btn btn-md btn-social btn-facebook"><span class="fa fa-facebook"></span> Facebook</a>
+		  <a href="#" class="btn btn-md btn-social btn-twitter"><span class="fa fa-twitter"></span> Twitter</a>
+		  <a href="#" class="btn btn-md btn-social btn-github"><span class="fa fa-github"></span> Github</a>
+		  <a href="#" class="btn btn-md btn-social btn-google"><span class="fa fa-google"></span> Google+</a>
+		</div>
+	</div>
+	<div class="col-md-4" style="background:rgba(44,62,80,0.8);border-right:1px solid #7f8c8d; height:230px;">
+		<span style="color:#FFF;font-size:32px;"><strong>Contacto</strong></span>
+		<form action="#">
+			<div class="form-group">
+				<label for="" class="sr-only">Nombre</label>
+				<input type="text" name="nombre" class="form-control" placeholder="Nombre">
+			</div>
+			<div class="form-group">
+				<label for="" class="sr-only">Mensaje</label>
+				<input type="textarea" name="mensaje" class="form-control" placeholder="Su mensaje">
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-linkedin">Enviar</button>
+			</div>
+
+		</form>
+	</div>
+	<div class="col-md-4" style="background:rgba(72,53,94,0.8); height:230px;">
+		<span style="color:#FFF;font-size:32px;"><strong>Menú</strong></span>
+		<div class="list-group">
+		  <a href="#" class="list-group-item">Inicio</a>
+		  <a href="#" class="list-group-item">Portafolios</a>
+		  <a href="#" class="list-group-item">Contacto</a>
+		  <a href="#" class="list-group-item">Creditos</a>
+		</div>
+	</div>
+</footer>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ paginacion/portfolio.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Desarrolladores de sitios web.</title>
+</head>
+<body>
+
+<header class="col-md-12">
+	<div class="col-md-8">
+		<h1>Purefull</h1>
+	</div>
+	<div class="col-md-4"><br>
+		<ul class="nav nav-pills">
+		  <li role="presentation"><a href="#">Home</a></li> 
+		  <li role="presentation"><a href="#">Profile</a></li> 
+		  <li role="presentation"><a href="#">Messages</a></li> 
+		</ul>
+	</div>
+</header>
+<div class="pedazo col-md-12">
+	<div class="row presentacion">
+		<div class="col-md-4">
+			<div class="title col-md-12"><h1><span class="glyphicon glyphicon-blackboard"></span> Presentación</h1></div>
+			<p><br><br><span class="glyphicon glyphicon-hand-right"></span> Diseño temático a pedido.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Informacion completa de su empresa<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Dominio .com.ve por un año.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Alojamiento mínimo.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Posicionamiento básico en SEO. <br><br>
+			<button type="button" class="btn btn-default pull-right">Más información</button></p>
+		</div>
+		<div class="col-md-4">
+			<div class="title col-md-12"><h1><span class="glyphicon glyphicon-comment"></span> Blog/Noticias</h1></div>
+			<p><br><br><span class="glyphicon glyphicon-hand-right"></span> Aplicacion de publicaciones personalizadas.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Diseño adaptable a moviles.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Dominio .com.ve por un año.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Alojamiento mínimo de 10gb.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Posicionamiento en SEO. <br><br>
+			<button type="button" class="btn btn-default pull-right">Más información</button></p>
+		</div>
+		<div class="col-md-4">
+			<div class="title col-md-12"><h1><span class="glyphicon glyphicon-shopping-cart"></span> E-comerce</h1></div>
+			<p><br><br><span class="glyphicon glyphicon-hand-right"></span> Aplicacion de carrito de compras.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Diseño adaptable a moviles.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Dominio .com.ve por un año.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Alojamiento mínimo de 10gb.<hr>
+			<span class="glyphicon glyphicon-hand-right"></span> Posicionamiento en SEO. <br><br>
+			<button type="button" class="btn btn-default pull-right">Más información</button></p>
+		</div>
+	</div>
+</div>
+</section>
+<div class="divider col-md-12"><h1><span class="glyphicon glyphicon-folder-close"></span> Portafolios</h1></div>
+<section class="col-md-12"><br><br>
+	<div class="row">
+	 <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+	    
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+	    
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+	    
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+
+	  <div class="col-sm-6 col-md-4">
+	    <div class="portfolio">
+	    	<img src="http://paginacion.com.ve/img/launion.jpg" alt="">
+	    	<p><center><big>Autorepuestos la union</big></center></p>
+	    	<span class="boton"><a href="#">Ver sitio</a></span>
+	    </div>
+	  </div>
+
+	</div>
+
+</section>
+<footer class="final">
+	<div class="col-md-4" style="background:rgba(72,53,94,0.8);border-right:1px solid #7f8c8d; height:230px;">
+		<span style="color:#FFF;font-size:32px;"><strong>Redes</strong></span>
+		<div social class="btn-group-vertical btn-block" role="group">
+		  <a href="#" class="btn btn-md btn-social btn-facebook"><span class="fa fa-facebook"></span> Facebook</a>
+		  <a href="#" class="btn btn-md btn-social btn-twitter"><span class="fa fa-twitter"></span> Twitter</a>
+		  <a href="#" class="btn btn-md btn-social btn-github"><span class="fa fa-github"></span> Github</a>
+		  <a href="#" class="btn btn-md btn-social btn-google"><span class="fa fa-google"></span> Google+</a>
+		</div>
+	</div>
+	<div class="col-md-4" style="background:rgba(44,62,80,0.8);border-right:1px solid #7f8c8d; height:230px;">
+		<span style="color:#FFF;font-size:32px;"><strong>Contacto</strong></span>
+		<form action="#">
+			<div class="form-group">
+				<label for="" class="sr-only">Nombre</label>
+				<input type="text" name="nombre" class="form-control" placeholder="Nombre">
+			</div>
+			<div class="form-group">
+				<label for="" class="sr-only">Mensaje</label>
+				<input type="textarea" name="mensaje" class="form-control" placeholder="Su mensaje">
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-linkedin">Enviar</button>
+			</div>
+
+		</form>
+	</div>
+	<div class="col-md-4" style="background:rgba(72,53,94,0.8); height:230px;">
+		<span style="color:#FFF;font-size:32px;"><strong>Menú</strong></span>
+		<div class="list-group">
+		  <a href="#" class="list-group-item">Inicio</a>
+		  <a href="#" class="list-group-item">Portafolios</a>
+		  <a href="#" class="list-group-item">Contacto</a>
+		  <a href="#" class="list-group-item">Creditos</a>
+		</div>
+	</div>
+</footer>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
